fd5551f06b76133b1e2256fcce34fb06dd809224
Mister Nemo <miister.nemo@gmail.com>
2019-11-15 17:18:55 +0200

    Change go-systray for temporary

--- box/const.go
+++ box/const.go
@@ -4,7 +4,8 @@ import (
 	"gearbox/eventbroker/msgs"
 	"gearbox/eventbroker/states"
 	//	oss "gearbox/os_support"
-	"github.com/gearboxworks/go-systray"
+	//"github.com/gearboxworks/go-systray"
+	"github.com/mister-nemo/go-systray" //!!!Important: this is for temporary
 	"time"
 )
 
